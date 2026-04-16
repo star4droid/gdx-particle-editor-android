ad74a04a5d82d6db4168cdefbc28d923db876ced
star4droid <ans44029@gmail.com>
2026-04-16 08:08:25 +0200

    fix PendingIntent mutability flag

--- android/src/main/java/com/star4droid/particle/editor/Star2dApp.java
+++ android/src/main/java/com/star4droid/particle/editor/Star2dApp.java
@@ -9,49 +9,50 @@ import android.os.Process;
 import android.util.Log;
 
 public class Star2dApp extends Application {
-  private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
-  private static Context mApplicationContext;
 
-  public static Context getContext() {
-    return mApplicationContext;
-  }
+    private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+    private static Context mApplicationContext;
 
-  public void onCreate() {
-    mApplicationContext = this.getApplicationContext();
-    this.uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
-    Thread.setDefaultUncaughtExceptionHandler(
-        new Thread.UncaughtExceptionHandler() {
+    public static Context getContext() {
+        return mApplicationContext;
+    }
 
-          public void uncaughtException(Thread thread, Throwable throwable) {
-            final Intent intent = new Intent(mApplicationContext, DebugActivity.class);
-            intent.putExtra("error", Log.getStackTraceString(throwable));
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            PendingIntent pendingIntent =
-                PendingIntent.getActivity(
-                    getApplicationContext(), 11111, intent, PendingIntent.FLAG_ONE_SHOT);
-            int x = 0;
-            String str =
-                getExternalFilesDir(null)
-                    .getAbsolutePath()
-                    .concat("/logs/log")
-                    .concat("%1$s")
-                    .concat(".txt");
-            while (FileUtil.isExistFile(str)) {
-              x++;
+    public void onCreate() {
+        mApplicationContext = this.getApplicationContext();
+        this.uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(
+                new Thread.UncaughtExceptionHandler() {
+
+            public void uncaughtException(Thread thread, Throwable throwable) {
+                final Intent intent = new Intent(mApplicationContext, DebugActivity.class);
+                intent.putExtra("error", Log.getStackTraceString(throwable));
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                PendingIntent pendingIntent
+                        = PendingIntent.getActivity(
+                                getApplicationContext(), 11111, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+                int x = 0;
+                String str
+                        = getExternalFilesDir(null)
+                                .getAbsolutePath()
+                                .concat("/logs/log")
+                                .concat("%1$s")
+                                .concat(".txt");
+                while (FileUtil.isExistFile(str)) {
+                    x++;
+                }
+                String log = Log.getStackTraceString(throwable);
+                // if(!FileUtil.readFile(String.format(str,(x-1)+"")).equals(log))
+                FileUtil.writeFile(String.format(str, String.valueOf(x)), log);
+                AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, pendingIntent);
+                Process.killProcess(Process.myPid());
+                mApplicationContext.startActivity(intent);
+                // System.exit(1);
+                // Logger.initialize(this);
+                Star2dApp.this.uncaughtExceptionHandler.uncaughtException(thread, throwable);
             }
-            String log = Log.getStackTraceString(throwable);
-            // if(!FileUtil.readFile(String.format(str,(x-1)+"")).equals(log))
-            FileUtil.writeFile(String.format(str, String.valueOf(x)), log);
-            AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-            am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, pendingIntent);
-            Process.killProcess(Process.myPid());
-            mApplicationContext.startActivity(intent);
-            // System.exit(1);
-            // Logger.initialize(this);
-            Star2dApp.this.uncaughtExceptionHandler.uncaughtException(thread, throwable);
-          }
         });
 
-    super.onCreate();
-  }
+        super.onCreate();
+    }
 }
